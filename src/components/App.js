5988fbe594e0b87c736c98380036fbe2d88cb07e
matthew rendely <matthew.rendely@gmail.com>
2023-04-08 11:56:23 -0700

    done

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,19 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About(){
+  return (
+    <h2 id='about'>About</h2>
+  )
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
